1f4aba0515e4c17189da54fc3dd6165d55ea8c7b
Mike Pellegrino <47326270+mkpellegrino@users.noreply.github.com>
2020-01-26 10:26:21 -0500

    Update disect.cpp

--- disect.cpp
+++ disect.cpp
@@ -6,15 +6,13 @@ int main(int argc, char *argv[])
 {
   FILE *binary = fopen(argv[1], "rb");
   unsigned char buffer;
-  int s = 16;
-  if( argc == 3 ) s = atoi(argv[2]);
-
+  
   cout << hex << uppercase;
   
-  while( fread(&buffer, sizeof(unsigned char), sizeof(buffer)/* /sizeof(*buffer)*/, binary) )
+  while( fread(&buffer, sizeof(unsigned char), sizeof(buffer), binary) )
     {
+      if( buffer < 0x10 ) cout << "0";
       cout << (int) buffer << " ";      
     }
-  
   return 0;
 }

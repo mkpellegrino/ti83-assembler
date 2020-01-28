eeff1758d56d102d98c6579462f42e6a97f9aa37
Mike Pellegrino <47326270+mkpellegrino@users.noreply.github.com>
2020-01-28 14:40:23 -0500

    Added compile functionality
    The First 3 rows of Opcodes and some SysCalls are in.  Syntax is starting to develop.

--- create-asm-prog.cpp
+++ create-asm-prog.cpp
@@ -1,93 +1,481 @@
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
+#include <vector>
+
 using namespace std;
-#define SZ 80
-int main(int argc, char *argv[])
+
+vector<unsigned char> byte_vector;
+
+int memorylocation;
+int start_counting=0;
+int program_index=0;
+
+class label;
+
+vector<label> label_vector;
+vector<label> label_cr_vector;
+
+class label
 {
-  FILE *binary = fopen(argv[1], "wb");
-  unsigned char buffer[SZ+1]=
-    {
-      '*','*','T','I','8','3','F','*', /* 0x00 - 0x07 */  
-      0x1A,0x0A,0x00, /* 0x08 - 0x0A */  
-      
-      'A','u','t','h','o', /* 0x0B - 0x34 (42 bytes) */
-      'r',' ','i','n','f','o',':',' ','h','t','t','p',':','/','/','m',
-      'k','p','e','l','l','e','g','.','f','r','e','e','s','h','e','l',
-      'l','.','o','r','g',
-      
-      0x18, 0x00, /* file length - 57d */  /*  len of prog + 19  in 2 bytes */
-      0x0D, 0x00, /* unknown characters */
-      0x07, 0x00, /* len of prog + 2 bytes */
-      0x06, /* protection */
-      'T','I','T','E','S','T','0','2',
-      0x00,0x00, /* two nulls */
-      (SZ-73),0x00, /* len of prog + 2 bytes */
-      0x05,0x00, /* len of prog */
-      0xBB,0x6D, /* program header */
-      0xC9, /* code */
-      0xC9, /* code */
-      0xC9, /* code */
-      0x4D,0x04 /* checksum */
-      
+public:
+  label()
+  {
+    index=program_index;
+    memory_location=memorylocation;
+    label_number=-1;
+#ifdef DEBUG
+    cerr << "creating label: [" << hex << memory_location << "]" << endl;
+#endif
   };
+  label(int l)
+  {
+    index=program_index;
+    memory_location=memorylocation;
+    label_number=l;
+#ifdef DEBUG
+    cerr << "creating label: [prg]" << dec << index << " links to label#" << dec << label_number << endl;
+#endif
+  };
+  ~label(){};
+  int getIndex(){ return index; };
+  int getMemoryLocation(){ return memory_location; };
+  int gerLabelNumber(){ return label_number; };
+  void add()
+  {
+#ifdef DEBUG
+    cerr << "changing bytes: [" << index << " and " << index+1 << "] to ["
+	 << hex << (label_cr_vector[label_number-1].getMemoryLocation()&0xFF) << " and "
+	 << hex << ( (label_cr_vector[label_number-1].getMemoryLocation()&0xFF00 )/0xFF) << "]" <<  endl;
+#endif 
+    byte_vector[index] = (label_cr_vector[label_number-1].getMemoryLocation()&0xFF);// L
+    byte_vector[index+1] = ((label_cr_vector[label_number-1].getMemoryLocation()&0xFF00)/0xFF );// H
+  };
+private:
+  int index;
+  int memory_location;
+  int label_number;
+};
 
-  int cs=0;
-  // calculate the checksum
-  for( int i=55; i<(SZ-2); i++ )
-    {
-      cs+=buffer[i];
-    }
 
-
-  int csL, csH=0;
-
-  csL = cs&0xFF;
-
-  csH = (cs & 0xFF00)/0xFF;
-
-  buffer[79]=csL;
-  buffer[80]=csH;
-  
-  for( int i=0; i<81; i++ )
-    {
-      cout << i << ": " << buffer[i] << endl;
-    }
-  
-  fwrite(&buffer, sizeof(unsigned char), sizeof(buffer), binary);
-  return 0;
+void startCounting()
+{
+  start_counting=1;
+  memorylocation=40341;
 }
 
+void addLabel()
+{
+  label l = label();
+  label_cr_vector.push_back( l );
+}
 
-/* 
+void addLabel( int i )
+{
+  label l = label(i);
+  label_vector.push_back( l );
+}
 
-Offset(hex)
+void addByte( unsigned char b )
+{
+  program_index++;
+  if( start_counting == 1) memorylocation++;  
+  byte_vector.push_back( (unsigned char) b );
+}
 
-0x00 - 0x07: "**TI83F*"
-0x08 - 0x0B: 0x1A, 0x0A, 0x00
-0x0B - 0x34: Comment
-0x35 - 0x36: File length - $39 = Size of all data in the .8xx file from byte $37 to last byte before checksum
-Variable Header
+void addWord( int w )
+{
+  addByte( w&0xFF );
+  addByte( (w&0xFF00 )/0xFF );
+}
+void addWord()
+{
+  for( int i=0; i<2; i++ )
+    {
+      program_index++;
+      if( start_counting == 1) memorylocation++;  
+      byte_vector.push_back( (unsigned char) 0x00 );
+    }
+}
+void addString( string s )
+{
+  for( int i=0; i<s.size(); i++ )
+    {
+      addByte( s[i] );
+    }
+  if( start_counting == 1 ) addByte( 0x00 );
+}
 
-0x37 - 0x38: Variable header length = 0B 00
-0x39 - 0x41: Variable Header
+void setComment( string s )
+{
+  s.resize(42,0x00);
+  addString(s);
+  
+}
+void setName( string s )
+{
+  s.resize(8,0x00);
+  cerr << "NAME: " << s << endl;
+  addString(s);
+}
 
-    0x39 - 0x3A: Length of data (word)
-    0x3B : Program Type - 5=Unprotected, 6= Protected
-    0x3C - 0x43: Program Name (0-filled)
+void sysCall( string s )
+{
+    addByte( 0xEF ); // rst $28
+    if( s == "PutS")
+      {
+	addWord(0x450A);
+      }
+    else if( s == "NewLine" )
+      {
+	addWord(0x452E);
+      }
+    else if( s == "DispHL" )
+      {
+	addWord(0x4507);
+      }
+    else if( s == "CpOP1ToOP2" )
+      {
+	addWord(0x4111);
+      }
+    else if( s == "CpOP4ToOP3" )
+      {
+	addWord(0x4108);
+      }
+    else if( s == "Mov9ToOP1" )
+      {
+	addWord(0x417A);
+      }
+    else if( s == "Mov9ToOP2" )
+      {
+	addWord(0x4180);
+      }
+    else if( s == "Mov9B" )
+      {
+	addWord(0x415F);
+      }
+    else if( s == "OP1ExOP2" )
+      {
+	addWord(0x421F);
+      }
+    else if( s == "RandInit" )
+      {
+	addWord(0x4B7F);
+      }
+    else if( s == "Random" )
+      {
+	addWord(0x4B79);
+      }
+    else if( s == "RclX" )
+      {
+	addWord(0x4AE0);
+      }
+    else if( s == "StoX" )
+      {
+	addWord(0x4AD1);
+      }
 
-Data
+}
 
-0x44 - 0x45: Length of data
-0x46 - 0x47: Length of program
-0x50 - 0x??: Program Data
-0x?? - EOF: Checksum (word)
+void assem( string s )
+{
+  if( s == "ret" )
+    {
+      addByte( 0xC9 );
+    }
+  else if( s == "ld hl,**" )
+    {
+      addByte( 0x21 );
+    }
+  else if( s == "nop" )
+    {
+      addByte( 0x00 );
+    }
+  else if( s == "ld bc, **" )
+    {
+      addByte(0x01);
+    }
+  else if( s == "ld (bc), a" )
+    {
+      addByte(0x02);
+    }
+  else if( s == "inc bc" )
+    {
+      addByte(0x03);
+    }
+  else if( s == "inc b" )
+    {
+      addByte(0x04);
+    }
+  else if( s == "dec b" )
+    {
+      addByte(0x05);
+    }
+  else if( s == "ld b, *" )
+    {
+      addByte(0x06);
+    }
+  else if( s == "rlca" )
+    {
+      addByte(0x07);
+    }
+  else if( s == "ex af, af'" )
+    {
+      addByte(0x08);
+    }
+  else if( s == "add hl, bc" )
+    {
+      addByte(0x09);
+    }
+  else if( s == "ld a, (bc)" )
+    {
+      addByte(0x0A);
+    }
+  else if( s == "dec bc" )
+    {
+      addByte(0x0B);
+    }
+  else if( s == "inc c" )
+    {
+      addByte(0x0C);
+    }
+  else if( s == "dec c" )
+    {
+      addByte(0x0D);
+    }
+  else if( s == "ld c, *" )
+    {
+      addByte(0x0E);
+    }
+  else if( s == "rrca" )
+    {
+      addByte(0x0F);
+    }
+  else if( s == "djnz *" )
+    {
+      addByte(0x10);
+    }
+  else if( s == "ld de, **" )
+    {
+      addByte(0x11);
+    }
+  else if( s == "ld (de), a" )
+    {
+      addByte(0x12);
+    }
+  else if( s == "inc de" )
+    {
+      addByte(0x13);
+    }
+  else if( s == "inc d" )
+    {
+      addByte(0x14);
+    }
+  else if( s == "dec d" )
+    {
+      addByte(0x15);
+    }
+  else if( s == "ld d, *" )
+    {
+      addByte(0x16);
+    }
+  else if( s == "rla" )
+    {
+      addByte(0x17);
+    }
+  else if( s == "jr *" )
+    {
+      addByte(0x18);
+    }
+  else if( s == "add hl, de" )
+    {
+      addByte(0x19);
+    }
+  else if( s == "ld a, (de)" )
+    {
+      addByte(0x1A);
+    }
+  else if( s == "dec de" )
+    {
+      addByte(0x1B);
+    }
+  else if( s == "inc e" )
+    {
+      addByte(0x1C);
+    }
+  else if( s == "dec e" )
+    {
+      addByte(0x1D);
+    }
+  else if( s == "ld e, *" )
+    {
+      addByte(0x1E);
+    }
+  else if( s == "rra" )
+    {
+      addByte(0x1F);
+    }
+  else if( s == "jr nz, *" )
+    {
+      addByte(0x20);
+    }
+  else if( s == "ld hl, **" )
+    {
+      addByte(0x21);
+    }
+  else if( s == "ld (**), hl" )
+    {
+      addByte(0x22);
+    }
+  else if( s == "inc hl" )
+    {
+      addByte(0x23);
+    }
+  else if( s == "inc h" )
+    {
+      addByte(0x24);
+    }
+  else if( s == "dec h" )
+    {
+      addByte(0x25);
+    }
+  else if( s == "ld h, *" )
+    {
+      addByte(0x26);
+    }
+  else if( s == "daa" )
+    {
+      addByte(0x27);
+    }
+  else if( s == "jr z, *" )
+    {
+      addByte(0x28);
+    }
+  else if( s == "add hl, hl" )
+    {
+      addByte(0x29);
+    }
+  else if( s == "ld hl, (**)" )
+    {
+      addByte(0x2A);
+    }
+  else if( s == "dec hl" )
+    {
+      addByte(0x2B);
+    }
+  else if( s == "inc l" )
+    {
+      addByte(0x2C);
+    }
+  else if( s == "dec l" )
+    {
+      addByte(0x2D);
+    }
+  else if( s == "ld l, *" )
+    {
+      addByte(0x2E);
+    }
+  else if( s == "cpl" )
+    {
+      addByte(0x2F);
+    }
+  else if( s == "" )
+    {
+      addByte(0x00);
+    }
 
-*/
+}
+int main(int argc, char *argv[])
+{  
+  FILE *binary = fopen(argv[1], "wb");
 
-// Byte 0x39 and 0x3A are Low Order then High Order
-// of End of file minus 0x49 (73 decimal)
-// 0x3C - 0x43 Program Name Zero Filled)
+  addString( "**TI83F*" );
+  addByte( 0x1A );
+  addByte( 0x0A );
+  addByte( 0x00 );
+  setComment( "http://mkpelleg.freeshell.org" );
+  addByte( 0xFF ); // PROGRAM LENGTH - 57 (L)  (53) (This will get overwritten)
+  addByte( 0xFF ); // PROGRAM LENGTH - 57 (H)  (54) (This will get overwritten)
+  addByte( 0x0D ); 
+  addByte( 0x00 );
 
-// The last two bytes are always a checksum of the data section, which always confused me, but is apparently just lowest 16 bits of the sum of all the data.
-//http://www.ticalc.org/archives/files/fileinfo/247/24750.html does a pretty good job detail this all. 
+  // PROGRAM START
+  addByte( 0xFF ); // len of Machine Code + 2 bytes (L) (This will get overwritten)
+  addByte( 0xFF ); // len of Machine Code + 2 bytes (H) (This will get overwritten)
+  addByte( 0x06 );
+  setName( "TEST1" ); // MUST BE 8 BYTES!!!
+  addByte( 0x00 );
+  addByte( 0x00 );
+  addByte( 0xFF ); // len of Machine Code + 2 bytes (L)  (This will get overwritten)
+  addByte( 0xFF ); // len of Machine Code + 2 bytes (H) (This will get overwritten)
+  addByte( 0xFF ); // Machine Code Size (L)  (This will get overwritten)
+  addByte( 0xFF ); // Machine Code Size (H) (This will get overwritten)
+
+  addByte( 0xBB ); // Machine Code
+  addByte( 0x6D );
+  startCounting();
+  // ==================================================
+  assem( "ld hl, **" ); addLabel(1); addWord();
+  sysCall("PutS");
+  
+  sysCall("NewLine");
+
+  assem( "ld hl, **"); addLabel(2); addWord();
+  sysCall("PutS");
+
+  sysCall("NewLine");
+
+
+  assem( "ld hl, **"); addWord( 0x1234 );
+  sysCall( "DispHL" );
+  
+  assem( "ret" );
+
+  // $9D9F
+  addLabel(); addString( "Written in Asm" );
+  addLabel(); addString( "x:" );
+
+  // ==================================================
+  // redo labels
+  for( int i=0; i<label_vector.size(); i++ )
+    {
+      label_vector[i].add();
+    }
+  
+  // Set Program Size-57
+  int psH,psL=0;
+  psH=((byte_vector.size()-55) & 0xFF00)/0xFF; // it's -55 (instead of -57) because we haven't added in the Checksum yet
+  psL=((byte_vector.size()-55) & 0xFF); // it's -55 (instead of -57) because we haven't added in the Checksum yet
+  byte_vector[53]=psL;
+  byte_vector[54]=psH;
+
+  // Set Machine Code Sizes -74
+  int mcsH,mcsL=0;
+  mcsH=((byte_vector.size()-72) & 0xFF00)/0xFF; // it's -72 (instead of -74) because we haven't added in the Checksum yet
+  mcsL=((byte_vector.size()-72) & 0xFF); // it's -72 (instead of -74) because we haven't added in the Checksum yet
+  byte_vector[57]=mcsL;
+  byte_vector[58]=mcsH;
+  byte_vector[70]=mcsL;
+  byte_vector[71]=mcsH;
+  // Set Just the Code Size
+  mcsH=((byte_vector.size()-74) & 0xFF00)/0xFF;
+  mcsL=((byte_vector.size()-74) & 0xFF);
+  byte_vector[72]=mcsL;
+  byte_vector[73]=mcsH;
+  
+  // calculate the checksum
+  int cs=0;
+  for( int i=55; i<byte_vector.size(); i++ )
+    {
+      cs+=byte_vector[i];
+    }
+  addByte( cs&0xFF );
+  addByte( (cs & 0xFF00)/0xFF );
+
+  for( int i=0; i<byte_vector.size(); i++ )
+    {
+      unsigned char byte_to_write = byte_vector[i];
+      fwrite(&byte_to_write, 1, sizeof(byte_to_write), binary);
+    }
+  return 0;
+}
